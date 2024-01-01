fe64a3f733c6116d7cf3232efe213edb1cfda666
tarekdinaji <mtarekhossain@gmail.com>
2024-01-01 19:36:00 +0600

    create and update done

--- app/controllers/shop_details_controller.rb
+++ app/controllers/shop_details_controller.rb
@@ -11,34 +11,37 @@ class ShopDetailsController < ApplicationController
     find_shop_detail
 
     render json: @shop_detail
-  end 
-
-  def new 
-    @shop_detail = ShopDetail.new
-  end 
-
-  def create 
-    shop_detail = ShopDetail.new(shop_detail_params)
-
-    if shop_detail.save 
-      render json: shop_detail
-    else 
-      render json: shop_detail.errors
-    end 
-    
   end  
 
+  def create 
+    @new_shop = ShopDetail.new(shop_name: params[:shop_name], country: params[:country], address_line_1: params[:address_line_1], address_line_2: params[:address_line_2], zipcode: params[:zipcode], country_code: params[:country_code], google_map_location: params[:google_map_location])
+
+    if @new_shop.save 
+      render json: @new_shop
+    else 
+      render json: @new_shop.errors
+    end 
+  end  
+
+  def update
+    @shop_to_update = ShopDetail.find(params[:id])
+    if @shop_to_update.update(shop_name: params[:shop_name], country: params[:country], address_line_1: params[:address_line_1], address_line_2: params[:address_line_2], zipcode: params[:zipcode], country_code: params[:country_code], google_map_location: params[:google_map_location]) 
+      render json: @shop_to_update
+    else 
+      render json: @shop_to_update.errors
+    end 
+  end  
 
   private 
 
    def find_shop_detail
     @shop_detail = ShopDetail.find(params[:id])
    end 
-
-   def shop_detail_params
-    #using strong parameters
-    params.require(:shop_detail).permit([:shop_name, :country, :address_line_1, :address_line_2, :zipcode, :country_code, :google_map_location, :phone_number ])
-   end
+  # Can not use strong params because of API issues
+  #  def shop_detail_params
+  #   #using strong parameters
+  #   params.permit(:shop_name, :country, :address_line_1, :address_line_2, :zipcode, :country_code, :google_map_location, :phone_number)
+  #  end
 
 end
 
